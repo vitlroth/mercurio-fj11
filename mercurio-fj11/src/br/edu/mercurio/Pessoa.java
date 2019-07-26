696598c0dd50e6438485b62e3f4711c0a6d21fb1
Luis Roth <vitlroth@gmail.com>
2019-07-26 13:00:11 -0300

    * Incluido método getMostraView

--- mercurio-fj11/src/br/edu/mercurio/Pessoa.java
+++ mercurio-fj11/src/br/edu/mercurio/Pessoa.java
@@ -21,11 +21,20 @@ public void getStringPessoa(){
 	this.pessoa = "Ola";
 	System.out.println(this.getPessoa());
 }
+
+
+
+
+public void getMostraView() {
+	System.out.println("ola mundo");
+}
 	
 	public static void main(String[] args) {
 		Pessoa p = new Pessoa();
 		p.getStringPessoa();
 	
 	}
+	
+	
 	
 }
