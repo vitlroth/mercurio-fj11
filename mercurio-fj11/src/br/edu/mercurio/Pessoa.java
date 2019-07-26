f8cce5fecfca0d8b076c66d751417af367a530a4
Luis Roth <vitlroth@gmail.com>
2019-07-26 12:34:09 -0300

    * Criado alguns métodos de acesso

--- mercurio-fj11/src/br/edu/mercurio/Pessoa.java
+++ mercurio-fj11/src/br/edu/mercurio/Pessoa.java
@@ -11,10 +11,21 @@ public class Pessoa {
 
 	
 	private String pessoa;
+	public String getPessoa() {
+		return pessoa;
+	}
 	
 	
-
+public void getStringPessoa(){
 	
+	this.pessoa = "Ola";
+	System.out.println(this.getPessoa());
+}
 	
+	public static void main(String[] args) {
+		Pessoa p = new Pessoa();
+		p.getStringPessoa();
+	
+	}
 	
 }
