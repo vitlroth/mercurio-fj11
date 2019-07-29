ea2381a34cf780f69409411cc4efafdee379aed8
Luis Roth <vitlroth@gmail.com>
2019-07-29 09:56:28 -0300

    * Criada classe Principal

---
+++ mercurio-fj11/src/br/edu/mercurio/Principal.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package br.edu.mercurio;
+
+/**
+ * @author vitlr
+ *
+ */
+public class Principal {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
